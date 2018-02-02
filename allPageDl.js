9698bab7277f617b5191e38d82917d51a8d2bd7a
oshige <oshige@mono-96.jp>
2018-02-02 14:27:13 +0900

    cleanup comment line

--- allPageDl.js
+++ allPageDl.js
@@ -6,9 +6,9 @@ find ./ -type f -name "*[content="width=1024"]*"
 grep [検索したい文字列] -rl [検索対象フォルダのパス]
 grep  width=1024  -rl ./
 grep  width=1024  -rl /Users/mono05/Documents/health/files/
-//                   ↓filename
+                      ↓filename
 find . | xargs grep -n hogehoge
-//                    <filetype>
+                      <filetype>
 find ./health.eek.jp/ -type f | xargs grep -n "width=1024"
 grep  width=1024  -rl ./
 */
@@ -19,21 +19,22 @@ var URL = require('url');
 var fs = require('fs');
 var path = require('path');
 
-//共通の設定
-//階層の指定
+// 共通の設定
+// 階層の指定
 var LINK_LEVEL = 3;
 
-//基準となるページURL
+// 基準となるページURL
 var TARGET_URL = "http://nodejs.jp/nodejs.org_ja/docs/v0.10/api/";
 var TARGET_URL = "https://health.dmkt-sp.jp/column/";
 var TARGET_URL = "http://do.local:8888/";
 var TARGET_URL = "https://www.rakuten.ne.jp/gold/shopjapan/";
-//sakura 
+// sakura 
 var TARGET_URL = "http://health-mgr.sakura.ne.jp/column";
 var TARGET_URL = "http://health-mgr.sakura.ne.jp/";
 var TARGET_URL = "http://health.eek.jp/";
 // D
-// var TARGET_URL = "https://health.dmkt-sp.jp/";//
+var TARGET_URL = "https://health.dmkt-sp.jp/";//
+var TARGET_URL = "https://health.dmkt-sp.jp/qa/";//
 client.set('browser', 'iphone');
   // 'ie' | 'edge' | 'chrome' | 'firefox' |
   // 'opera' | 'vivaldi' | 'safari' |
@@ -48,58 +49,46 @@ var exitCount = 5;
 var list = [], listPage = [];
 var count = 0;
 
-//メイン処理
+// メイン処理
 var totalCount = downloadRec(TARGET_URL, 0);
 console.log('total ' + totalCount);
 
-//downloadRec( "a aa "+ TARGET_URL, 0); //indexOfの動作確認
+// downloadRec( "a aa "+ TARGET_URL, 0); // indexOfの動作確認
 
-//指定のurlを最大レベルlevelまでダウンロードする
+// 指定のurlを最大レベルlevelまでダウンロードする
 function downloadRec(url, level) {
   if (!url.startsWith(TARGET_URL) && count>0) return;
   // if (url.startsWith(TARGET_URL) && count>0) return;
-  //最大レベルのチェックをする (最大レベルになるまでループさせるため)
+  // 最大レベルのチェックをする (最大レベルになるまでループさせるため)
   if (level >= LINK_LEVEL) return;
 
-  //既出のサイトは無視をする。
+  // 既出のサイトは無視をする。
   console.log(list);
-  // var matchData = list.filter(function(item, index){
-  //   if (item == url && count>0) return true;
-  // });
   var matchData = list.indexOf(url);
-
+  // マッチング用のURLリストを作成
   if (matchData  > 0 ) return true;
   else list.push(url);
-  // マッチング用のURLリストを作成
-
   console.log('matchData:'+ matchData);
-  // if (list[url]) return;
 
-  //基準ページ以外なら無視をする
-  //-----------------------------------------------------------
+  // 基準ページ以外なら無視をする // 入力::http://nodejs.jp/nodejs.org_ja/docs/v0.10/api
   var us = TARGET_URL.split("/");
-
-  //  console.log(us);  //出力::[ 'http:', '', 'nodejs.jp', 'nodejs.org_ja', 'docs', 'v0.10', 'api' ]
-
-  us.pop(); //popメソッドを使用して、配列の最後の要素を削除します。
-  //console.log(us.pop()); //出力::api
-
-  //  console.log(us);  //出力::[ 'http:', '', 'nodejs.jp', 'nodejs.org_ja', 'docs', 'v0.10' ]
-
-  var base = us.join("/"); //joinメソッドは配列の各要素を指定の文字列で連結し、結果の文字列を返します。
-  //  console.log(base);  //出力::http://nodejs.jp/nodejs.org_ja/docs/v0.10
-
+  // console.log(us);        // 出力::['http:','','nodejs.jp','nodejs.org_ja','docs','v0.10','api']
+  us.pop();                  // 出力::api 配列の最後の要素を削除
+  // console.log(us);        // 出力::['http:','', 'nodejs.jp', 'nodejs.org_ja', 'docs','v0.10']
+  var base = us.join("/");  //配列の各要素を指定の文字列で連結
+  //  console.log(base);     // 出力::http://nodejs.jp/nodejs.org_ja/docs/v0.10
   console.log('url : ' + url);
-  if (url.indexOf(base) < 0) return;
-  //console.log(url.indexOf(base));
-  //-----------------------------------------------------------
+  if (url.indexOf(base) < 0)
+    {console.log( 'this is other site');
+    return;}
+
   // HTMLを取得する
   // client.fetch(url, {}, function (err, $, res) {
   client.fetch(url, {})
   .then(function (res) {
     var $ =res.$;
     // client.fetchSync(url, {}, function (err, $, res) {
-    //リンクされているページを取得
+    // リンクされているページを取得
     console.log('$("a")' + $("a"));
     $("a").each(function (idx) {
       // console.log(idx);
@@ -118,7 +107,7 @@ function downloadRec(url, level) {
         // 前方一致のときの処理
         var metaViewport = '';
         metaViewport = $("meta[name='viewport']").attr();
-        if (!href) return; //href属性を取得できない時の処理
+        if (!href) return; // href属性を取得できない時の処理
         console.log('href ' + href);
         console.log('url + href ' + url + href);
         // console.log(metaViewport);
@@ -133,23 +122,19 @@ function downloadRec(url, level) {
           console.log('wat??');
           console.log(metaViewport.content);
         }
-
         // console.log('count:'+count);
 
-
+        // あるURLから 相対的なURL を 絶対URL へ生成する場合には resolve メソッドを使用します。
         // var URL = require('url');
-        // ある URL から相対的な URL を絶対URLへ生成する場合には resolve メソッドを使用します。
-        // URL.resolve(from, to)
-        // 例えば、あるページ("http://h.com/foo/index.html")　にリンク
-        //                 (<a href="../bar/baz.html">xxxx</a>) があったとして
-        // そのリンク先の URL はどんな値になるかというのを調べるのに使えます。
-        //  href = URL.resolve("http://h.com/foo/index.html", "../bar/baz.html");
-        //  echo href; //  http://h.com/bar/baz.html
+        // var from =  'http://h.com/foo/index.html' ,
+        //     to   =  '../bar/index.html' ;
+        // var href = URL.resolve( from , to );
+        // echo href; //http://h.com/bar/baz.html
         href = URL.resolve(url, href);
 
-        //'#' 以降を無視する(a.html#aa と a.html#bb　は同じものとする)
-        href = href.replace(/\#.+$/, ""); //末尾の#を消す
-        href = href.replace(/\?/, ""); //?を消す
+        // '#' 以降を無視する(a.html#aa と a.html#bb　は同じものとする)
+        href = href.replace(/\#.+$/, ""); // 末尾の#を消す
+        href = href.replace(/\?/, "");    // ?を消す
 
         downloadRec(href, level + 1);
 
@@ -162,9 +147,8 @@ function downloadRec(url, level) {
     if (url.substr(url.length - 1, 1) == '/') {
       url += "index.html"; // インデックスを自動追加する。
     }
-
-    // console.log(url.split("/")); //[ 'http:',  '',  'nodejs.jp',  'nodejs.org_j  'docs',  'v0.10',  'download' ];
-    // console.log(url.split("/").slice(2)); //[ 'nodejs.jp', 'nodejs.org_ja', 'docs', 'v0.10', 'download' ]
+    // console.log(url.split("/"));          //[ 'http:','','nodejs.jp','nodejs.org_ja','docs','v0.10','download']
+    // console.log(url.split("/").slice(2)); //[            'nodejs.jp','nodejs.org_ja','docs','v0.10','download']
 
     var savepath = url.split("/").slice(2).join("/"); //slice::配列の一部を取り出して新しい配列を返します。
     //  console.log(savepath);               // nodejs.jp/nodejs.org_ja/docs/v0.10/download
