5804c836a632af8325fe7998371137dbf6f450b1
oshige <oshige@mono-96.jp>
2018-02-05 17:49:45 +0900

    test promise and async/await

---
+++ awaitAsync.js
@@ -0,0 +1,26 @@
+// // Node8
+const util = require('util')
+const fs = require('fs')
+
+const readFile = util.promisify(fs.readFile);
+readFile('./package.json')
+  .then((json) => JSON.parse(json))
+  .then((json) => console.log(json))
+  .catch((e) => console.error('error!!', e)); // a.json が不正ならここで SyntaxError が出る
+
+
+// // async-await 版だとこうなります。
+
+// const util = require('util')
+// const fs = require('fs')
+
+// const readFile = util.promisify(fs.readFile)
+// async function parse() {
+//   try {
+//     const json = await readFile('./package.json')
+//     console.log(JSON.parse(json))
+//   } catch(e) {
+//     console.error('error!', e)
+//   }
+// }
+// parse()
